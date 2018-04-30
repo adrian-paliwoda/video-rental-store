454bf183b4c0befe91e9140a626a7e980f8579a5
Adrian Paliwoda <adrian9403@gmail.com>
2018-04-30 22:33:14 +0200

    routes and parameters

--- video_rental_store/video_rental_store/Controllers/MoviesController.cs
+++ video_rental_store/video_rental_store/Controllers/MoviesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using video_rental_store.Models;
 
 namespace video_rental_store.Controllers
@@ -10,9 +12,39 @@ namespace video_rental_store.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Index()
+        [Route("Movies/{pageIndex:int?}/{sortBy?}")]
+        [Route("Movies/Index/{pageIndex:int?}/{sortBy?}")]
+        public ActionResult Index(int? pageIndex, string sortBy)
         {
-            return View();
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            //return View();
+            return Content(String.Format("pageIndex: {0}, sortBy: {1}", pageIndex, sortBy));
+        }
+
+        [Route("Movies/released/{year:regex(\\d{4}):int?}/{month:range(1,12):int?}")]
+        public ActionResult ByReleaseDate(int? year, int? month)
+        {
+            if (!year.HasValue)
+            {
+                year = DateTime.Now.Year;
+            }
+
+            if (!month.HasValue)
+            {
+                month = DateTime.Now.Month;
+            }
+
+            return Content(year + " " + month);
+            //return View();
         }
         public ActionResult Random()
         {
@@ -20,6 +52,13 @@ namespace video_rental_store.Controllers
 
 
             return View(movie);
+        }
+
+        [Route("Movies/Edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            return Content("id =" + id);
+            //return View();
         }
     }
 }
