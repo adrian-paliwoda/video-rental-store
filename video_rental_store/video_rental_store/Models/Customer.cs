907cb0ffa5a95dfffd2280caa060c7a954a4d987
Adrian Paliwoda <adrian9403@gmail.com>
2018-05-01 18:08:49 +0200

    Membership type

--- video_rental_store/video_rental_store/Models/Customer.cs
+++ video_rental_store/video_rental_store/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace video_rental_store.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MemberShipType MemberShipType { get; set; }
+        public byte MemberShipTypeId { get; set; }
     }
 }
---
+++ video_rental_store/video_rental_store/Models/MemberShipType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace video_rental_store.Models
+{
+    public class MemberShipType
+    {
+        public byte Id { get; set; }
+        public short SignTypeFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+
+    }
+}
